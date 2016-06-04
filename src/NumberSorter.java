3400f437cb5ee5dc423f52b2dd63c32d292e10bd
Lucas Baizer <titaniumsapphire32@gmail.com>
2016-06-04 11:31:41 -0700

    NEW THINGS
    CODE4LIFE AND STUFF

---
+++ src/NumberSorter.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class NumberSorter {
+
+	public static void main(String[] args) {
+		String num1S = JOptionPane.showInputDialog("Enter a number.");
+		String num2S = JOptionPane.showInputDialog("Enter a second number.");
+		String num3S = JOptionPane.showInputDialog("Enter a third number.");
+		int num1I = Integer.parseInt(num1S);
+		int num2I = Integer.parseInt(num2S);
+		int num3I = Integer.parseInt(num3S);
+		if (num1I < num2I){
+		}
+		if (num1I < num3I){
+		}
+		if (num2I < num3I){
+		}
+		}
+	}
+				
+				
+			
+			
+		
